2139be64382a03d7414b79019be3c06cdd460505
Pwdrc Tclvc <pwdrc@protonmail.com>
2022-04-26 15:35:28 -0300

    Python Week 2022 - Day 2 - Part 1

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,5 +1,35 @@
-from .config import settings
+from curses import beep
+from beerlog.models import Beer
+import typer
+from typing import Optional
+from beerlog.core import add_beer_to_database, get_beers_from_database
+from rich.table import Table
+from rich.console import Console
 
+main = typer.Typer(help="Beer Management Application")
 
-def main():
-    print("Hello from", settings.NAME)
+@main.command("add")
+def add(
+    name: str, 
+    style: str,
+    flavor: int = typer.Option(...),
+    image: int = typer.Option(...),
+    cost: int = typer.Option(...)
+):
+    """Adds a new beer to database"""
+    if add_beer_to_database(name, style, flavor, image, cost):
+        print("Beer added to database")
+
+@main.command("list")
+def list_beers(style: Optional[str] = None):
+    """List beers in database"""
+    beers = get_beers_from_database()
+    table = Table(title="Beerlog")
+    headers = ["id", "name", "style", "rate", "date"]
+    for header in headers:
+        table.add_column(header, style="magenta")
+    for beer in beers:
+        values = [str(getattr(beers, header)) for header in headers]
+        table.add_row(*values)
+    console.print(table)
+
